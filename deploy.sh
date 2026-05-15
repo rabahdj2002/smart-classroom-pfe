e0c1c2b8ece2411e4e5489eaeb5935b5e349bdc0
Rabah Djebbes <39097998+rabahdj2002@users.noreply.github.com>
2026-05-15 12:03:18 +0100

    Delete deploy.sh

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "🚀 Deploying SmartHelmet Live Dashboard..."
-git add .
-git commit -m "Auto-deploy $(date)"
-git push origin main
-ssh -p 2222 rabah@105.235.135.90 "cd /var/www/smart-helmet-pfe && git pull origin main && cd /var/www/smart-helmet-pfe && python manage.py migrate && sudo systemctl restart smarthelmet-django.service && echo '✅ LIVE at https://dashboard.rabahdj.online!'"
