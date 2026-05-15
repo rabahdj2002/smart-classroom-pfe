08ab63f1388b772b5daa765e249bcc4687b934fc
Rabah Djebbes <39097998+rabahdj2002@users.noreply.github.com>
2026-05-15 12:03:02 +0100

    Delete backend directory

--- backend/dashboard/context_processors.py
+++
@@ -1,9 +0,0 @@
-from .models import SystemSettings, Incident
-
-def app_settings(request):
-    settings = SystemSettings.objects.first()
-    critical_incidents_count = Incident.objects.filter(resolved=False).count()
-    return {
-        'app_settings': settings,
-        'global_critical_incidents': critical_incidents_count
-    }
